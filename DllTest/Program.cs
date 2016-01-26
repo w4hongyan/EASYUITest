be81e595eac0492c009cba953fc24c30b2649590
Wenshen <w4hongyan@163.com>
2016-01-26 17:02:32 +0800

    Redis测试

--- DllTest/Program.cs
+++ DllTest/Program.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Tools;
 
@@ -18,12 +19,90 @@ namespace DllTest
             //list.Add(new string[]{"name","姓名"});
             //DataTable dt = GetTable();
             //NPOIHelper.CreateXls(@"D:\text.xls", "你好", dt,list);
-            TestGetMemCache();
+            //TestGetMemCache();
             //DataTable table= NPOIHelper.ToDataTable(@"D:\import.xls", "0", 0);
             //LogHelper.WriteLog(typeof(Program), "测试日志");
+            //List<string> list=RedisHelper.GetAllItems("QueueListId");
+            //foreach (var item in list)
+            //{
+            //  Console.WriteLine(item);  
+            //}
+            //Console.WriteLine(RedisHelper.GetAllItems("QueueListId"));
+            TestRedisObject();
             Console.WriteLine("ok");
 
             Console.ReadKey();
+        }
+        public static void TestRedisObject()
+        {
+
+            using (var cars = RedisHelper.GetTypedClient<Car>())
+            {
+                if (cars.GetAll().Count > 0)
+                    cars.DeleteAll();
+
+                var dansFord = new Car
+                {
+                    Id = cars.GetNextSequence(),
+                    Title = "Dan's Ford",
+                    Make = new Make { Name = "Ford" },
+                    Model = new Model { Name = "Fiesta" }
+                };
+                var beccisFord = new Car
+                {
+                    Id = cars.GetNextSequence(),
+                    Title = "Becci's Ford",
+                    Make = new Make { Name = "Ford" },
+                    Model = new Model { Name = "Focus" }
+                };
+                var vauxhallAstra = new Car
+                {
+                    Id = cars.GetNextSequence(),
+                    Title = "Dans Vauxhall Astra",
+                    Make = new Make { Name = "Vauxhall" },
+                    Model = new Model { Name = "Asta" }
+                };
+                var vauxhallNova = new Car
+                {
+                    Id = cars.GetNextSequence(),
+                    Title = "Dans Vauxhall Nova",
+                    Make = new Make { Name = "Vauxhall" },
+                    Model = new Model { Name = "Nova" }
+                };
+
+                var carsToStore = new List<Car> { dansFord, beccisFord, vauxhallAstra, vauxhallNova };
+                cars.StoreAll(carsToStore);
+
+                Console.WriteLine("Redis Has-> " + cars.GetAll().Count + " cars");
+
+                cars.ExpireAt(vauxhallAstra.Id, DateTime.Now.AddSeconds(5)); //Expire Vauxhall Astra in 5 seconds
+
+                Thread.Sleep(6000); //Wait 6 seconds to prove we can expire our old Astra
+
+                Console.WriteLine("Redis Has-> " + cars.GetAll().Count + " cars");
+
+                //Get Cars out of Redis
+                var carsFromRedis = cars.GetAll().Where(car => car.Make.Name == "Ford");
+
+                foreach (var car in carsFromRedis)
+                {
+                    Console.WriteLine("Redis Has a ->" + car.Title);
+                }
+
+            }
+            Console.ReadLine(); 
+        }
+        public static void TestRedisSet()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                RedisHelper.Set<string>("key" + i, i.ToString());
+            }
+        }
+
+        public static List<string> GetCity(string key)
+        {
+            return RedisHelper.Get<List<string>>(key);
         }
         public static void TestGetMemCache()
         {
@@ -53,5 +132,26 @@ namespace DllTest
             }
         }
 
+    }
+    public class Car
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Make Make { get; set; }
+        public Model Model { get; set; }
+    }
+
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Model
+    {
+        public int Id { get; set; }
+        public Make Make { get; set; }
+        public string Name { get; set; }
     }
 }
---
+++ Tools/RedisHelper.cs
@@ -0,0 +1,288 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Redis.Generic;
+
+namespace Tools
+{
+    public class RedisHelper:IDisposable
+    {
+        public static RedisClient redisClient = null;
+        static RedisHelper()
+        {
+            if (redisClient == null)
+            {
+                CreateClient("127.0.0.1", 6379);
+            }
+        }
+
+        public static void CreateClient(string hostIP, int port)
+        {
+            if (redisClient == null)
+            {
+                redisClient = new RedisClient(hostIP, port);
+            }
+
+        }
+        
+        public static T Get<T>(string key)
+        {
+            return redisClient.Get<T>(key);
+        }
+        public static bool Set<T>(string key,T value)
+        {
+           return redisClient.Set<T>(key, value);
+        }
+        public static byte[] getValueByte(string key)
+        {
+            byte[] value = redisClient.Get(key);
+            return value;
+        }
+        public static string getValueString(string key)
+        {
+            string value = redisClient.GetValue(key);
+            return value;
+        }
+        /// <summary>
+        /// 获得某个hash型key下所有字段
+        /// </summary>
+        /// <param name="hashId"></param>
+        /// <returns></returns>
+        public static List<string> GetHashFields(string hashId)
+        {
+            List<string> hashFileds = redisClient.GetHashKeys(hashId);
+            return hashFileds;
+        }
+        /// <summary>
+        /// 获得某个hash型key下所有值
+        /// </summary>
+        /// <param name="hashId"></param>
+        /// <returns></returns>
+        public static List<string> GetHashValues(string hashId)
+        {
+            List<string> hashFileds = redisClient.GetHashValues(hashId);
+            return hashFileds;
+        }
+         /// <summary>
+         /// 获得hash型key某个字段的值
+         /// </summary>
+         /// <param name="key"></param>
+         /// <param name="field"></param>
+         public static string GetHashField(string key, string field)
+         {
+             string value = redisClient.GetValueFromHash(key, field);
+             return value;
+         }
+         /// <summary>
+         /// 设置hash型key某个字段的值
+         /// </summary>
+         /// <param name="key"></param>
+         /// <param name="field"></param>
+         /// <param name="value"></param>
+         public static void SetHashField(string key, string field, string value)
+         {
+             redisClient.SetEntryInHash(key, field, value);
+         }
+         /// <summary>
+         ///使某个字段增加
+         /// </summary>
+         /// <param name="key"></param>
+         /// <param name="field"></param>
+         /// <returns></returns>
+         public static void SetHashIncr(string key, string field, long incre)
+         {
+             redisClient.IncrementValueInHash(key, field, incre);
+ 
+         }
+         /// <summary>
+         /// 向list类型数据添加成员，向列表底部(右侧)添加
+         /// </summary>
+         /// <param name="Item"></param>
+         /// <param name="list"></param>
+         public static void AddItemToListRight(string list, string item)
+         {
+             redisClient.AddItemToList(list, item);
+         }
+         /// <summary>
+         /// 向list类型数据添加成员，向列表顶部(左侧)添加
+         /// </summary>
+         /// <param name="list"></param>
+         /// <param name="item"></param>
+         public static void AddItemToListLeft(string list, string item)
+         {
+             redisClient.LPush(list, Encoding.Default.GetBytes(item));
+         }
+         /// <summary>
+         /// 从list类型数据读取所有成员
+         /// </summary>
+         public static List<string> GetAllItems(string list)
+         {
+             List<string> listMembers = redisClient.GetAllItemsFromList(list);
+             return listMembers;
+         }
+         /// <summary>
+         /// 从list类型数据指定索引处获取数据，支持正索引和负索引
+         /// </summary>
+         /// <param name="list"></param>
+         /// <returns></returns>
+         public static string GetItemFromList(string list, int index)
+         {
+             string item = redisClient.GetItemFromList(list, index);
+             return item;
+         }
+         /// <summary>
+         /// 向列表底部（右侧）批量添加数据
+         /// </summary>
+         /// <param name="list"></param>
+         /// <param name="values"></param>
+         public static void GetRangeToList(string list, List<string> values)
+         {
+             redisClient.AddRangeToList(list, values);
+         }
+         /// <summary>
+         /// 向集合中添加数据
+         /// </summary>
+         /// <param name="item"></param>
+         /// <param name="set"></param>
+         public static void GetItemToSet(string set, string item)
+         {
+             redisClient.AddItemToSet(set,item);
+         }
+         /// <summary>
+         /// 获得集合中所有数据
+         /// </summary>
+         /// <param name="set"></param>
+         /// <returns></returns>
+         public static HashSet<string> GetAllItemsFromSet(string set)
+         {
+             HashSet<string> items = redisClient.GetAllItemsFromSet(set);
+             return items;
+         }
+         /// <summary>
+         /// 获取fromSet集合和其他集合不同的数据
+         /// </summary>
+         /// <param name="fromSet"></param>
+         /// <param name="toSet"></param>
+         /// <returns></returns>
+         public static HashSet<string> GetSetDiff(string fromSet, params string[] toSet)
+         {
+             HashSet<string> diff = redisClient.GetDifferencesFromSet(fromSet, toSet);
+             return diff;
+         }
+         /// <summary>
+         /// 获得所有集合的并集
+         /// </summary>
+         /// <param name="set"></param>
+         /// <returns></returns>
+         public static HashSet<string> GetSetUnion(params string[] set)
+         {
+             HashSet<string> union = redisClient.GetUnionFromSets(set);
+             return union;
+         }
+         /// <summary>
+         /// 获得所有集合的交集
+         /// </summary>
+         /// <param name="set"></param>
+         /// <returns></returns>
+         public static HashSet<string> GetSetInter(params string[] set)
+         {
+             HashSet<string> inter = redisClient.GetIntersectFromSets(set);
+             return inter;
+         }
+         /// <summary>
+         /// 向有序集合中添加元素
+         /// </summary>
+         /// <param name="set"></param>
+         /// <param name="value"></param>
+         /// <param name="score"></param>
+         public static void AddItemToSortedSet(string set, string value, long score)
+         {
+             redisClient.AddItemToSortedSet(set, value, score);
+         }
+         /// <summary>
+         /// 获得某个值在有序集合中的排名，按分数的降序排列
+         /// </summary>
+         /// <param name="set"></param>
+         /// <param name="value"></param>
+         /// <returns></returns>
+         public static int GetItemIndexInSortedSetDesc(string set, string value)
+         {
+             int index = redisClient.GetItemIndexInSortedSetDesc(set, value);
+             return index;
+         }
+         /// <summary>
+         /// 获得某个值在有序集合中的排名，按分数的升序排列
+         /// </summary>
+         /// <param name="set"></param>
+         /// <param name="value"></param>
+         /// <returns></returns>
+         public static int GetItemIndexInSortedSet(string set, string value)
+         {
+             int index = redisClient.GetItemIndexInSortedSet(set, value);
+             return index;
+         }
+         /// <summary>
+         /// 获得有序集合中某个值得分数
+         /// </summary>
+         /// <param name="set"></param>
+         /// <param name="value"></param>
+         /// <returns></returns>
+         public static double GetItemScoreInSortedSet(string set, string value)
+         {
+             double score = redisClient.GetItemScoreInSortedSet(set, value);
+             return score;
+         }
+         /// <summary>
+         /// 获得有序集合中，某个排名范围的所有值
+         /// </summary>
+         /// <param name="set"></param>
+         /// <param name="beginRank"></param>
+         /// <param name="endRank"></param>
+         /// <returns></returns>
+         public static List<string> GetRangeFromSortedSet(string set, int beginRank, int endRank)
+         {
+             List<string> valueList = redisClient.GetRangeFromSortedSet(set, beginRank, endRank);
+             return valueList;
+         }
+         /// <summary>
+         /// 获得有序集合中，某个分数范围内的所有值，升序
+         /// </summary>
+         /// <param name="set"></param>
+         /// <param name="beginScore"></param>
+         /// <param name="endScore"></param>
+         /// <returns></returns>
+         public static List<string> GetRangeFromSortedSet(string set, double beginScore, double endScore)
+         {
+             List<string> valueList = redisClient.GetRangeFromSortedSetByHighestScore(set, beginScore, endScore);
+             return valueList;
+         }
+         /// <summary>
+         /// 获得有序集合中，某个分数范围内的所有值，降序
+         /// </summary>
+         /// <param name="set"></param>
+         /// <param name="beginScore"></param>
+         /// <param name="endScore"></param>
+         /// <returns></returns>
+         public static List<string> GetRangeFromSortedSetDesc(string set, double beginScore, double endScore)
+         {
+             List<string> vlaueList = redisClient.GetRangeFromSortedSetByLowestScore(set, beginScore, endScore);
+             return vlaueList;
+         }
+        /// <summary>
+         /// 获得强类型IRedisTypedClient
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+         public static IRedisTypedClient<T> GetTypedClient<T>()
+         {
+             return redisClient.As<T>();
+         }
+         public void Dispose()
+         {
+             redisClient.Dispose();
+         }
+    }
+}
